6cffcbe0a995f0c31a07360f4d4cecf9aa60467e
Thirumurugan B <thiruthiru33120@gmail.com>
2026-08-05 17:55:18 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (49.96%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,15 +14,10 @@
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root){
-        if(root==null){
-            return 0;
-        }
+    public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        if(left > right)
-            return left+1;
-        else 
-            return right+1;
+        return Math.max(left,right) + 1;
     }
 }
